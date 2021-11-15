a0226eb836805379085a3a20749a425bf0464615
OUMontiel <omarume@gmail.com>
2021-11-14 20:40:52 -0600

    Add functionality

--- app.js
+++ app.js
@@ -6,30 +6,26 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connection to db
-
 mongoose.connect('mongodb://localhost/crud-mongo-blog')
     .then(db => console.log('db connected'))
     .catch(err => console.log(err));
 
-// importing routes
-const indexRoutes = require('./routes/routeindex');
-
-
 // settings
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
+// importing routes
+const indexRoutes = require('./routes/routeindex');
 
 // routes
 app.use('/', indexRoutes);
 
-app.listen(app.get('port'), () =>{
+app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
 })
-
-
--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,18 +1,52 @@
 const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
-//const Task = require('../model/task');
+const Post = require('../model/post.js');
 
-
-router.get('/', async function(req,res){
-  res.render('index');
+router.get('/', async (req, res) => {
+  var posts = await Post.find()
+  res.render('index', {posts});
 });
 
-
-router.get('/newPost', async (req,res) =>{
+router.get('/newPost', async (req, res) => {
   res.render('newPost');
 });
 
+router.post('/newPost', async (req, res) => {
+  var post = new Post(req.body)
+  await post.save()
+  res.redirect('/')
+})
 
+router.get('/edit/:id', async (req, res) => {
+  var id = req.params.id
+  var posts = await Post.find()
+  var post = posts.find(post => post._id == id)
+  res.render('edit', {id, post})
+})
+
+router.post('/edit/:id', async (req, res) => {
+  var id = req.params.id
+  var posts = await Post.find()
+  var index = posts.findIndex((obj => obj.id == id))
+  posts[index].title = req.body.title
+  posts[index].author = req.body.author
+  posts[index].post_data = req.body.post_data
+  await posts[index].save()
+  res.redirect('/')
+})
+
+router.get('/delete/:id', async (req, res) => {
+  var id = req.params.id
+  var posts = await Post.find()
+  var post = posts.find(post => post._id == id)
+  res.render('delete', {id, post})
+})
+
+router.post('/delete/:id', async (req, res) => {
+  var id = req.params.id
+  await Post.remove({_id: id})
+  res.redirect('/')
+})
 
 module.exports = router;
